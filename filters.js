a5a1cea7f0bc5ec2dd5c84e3bd8a9977be8bfd51
fv0 <ahead@web.de>
2017-06-10 20:11:49 +0200

    Improved gluten filter with data from glutenchecker.ch

--- filters.js
+++ filters.js
@@ -1,43 +1,124 @@
 var filters = {
   "filter": [
 
-	{
-		name: "Gluten",
-		  "filter_exclusion": [
-			"Gluten", "Weizen", "Weizenmehl", "Weizenhalbweissmehl", "Weizenhalbweißmehl", "Weizenkorn", "Weizengluten", "Hartweizengrieß", "Hartweizengriess", "hartweizengrieß", "Hartweizengrieß*", "Hartweizen", "Harwiezengriess", "Hartweizengreiss", "Hartwiezen", "Weizenkeim", "Weizenkeime", "Weizenkörner", "Weizenstärke", "Weizenflocken", "Weizenkleie", "Weizenkleber", "Weizeneiweiß", "Weizeneiweiss", "Gerste", "Gerstenmalz", "Gerstenmalzextrakt", "Gerstenmalzmehl", "Roggen", "Roggenmehl", "Roggenvollkornschrot", "Roggenvollkornmehl", "Roggensauerteig", "Hafer", "Hafervollkorn", "Hafervollkornmehl", "Haferflocken", "Haferclocken", "Hafervollkornflocken", "Haferkleie", "Dinkel", "Dinkelvollkornnudeln", "Dinkelvollkornflocken", "Dinkelschrot", "Dinkelvollkornschrot", "Dinkelmehl", "UrDinkelgreiss", "Urdinkelgriess", "Dinkelgrieß", "Grünkern", "Einkorn", "Kamut", "Bulgur", "Emmer", "Triticale", "Weizeneiweiß", "Weizeneiweiss", "Weizenkleber", "Seitan", "Couscous"
-		  ],
-		  "filter_warning": [
-			"Sojasoße", "Sojasosse", "Sojasoßenpulver", "Sojasossenpulver"
-		  ]
-
+    {
+      name: "Gluten",
+      "filter_exclusion": [
+        "Bulgur",
+        "Couscous",
+        "Dinkel",
+        "Dinkelgrieß",
+        "Dinkelmehl",
+        "Dinkelschrot",
+        "Dinkelvollkornflocken",
+        "Dinkelvollkornmehl",
+        "Dinkelvollkornnudeln",
+        "Dinkelvollkornschrot",
+        "Einkorn",
+        "Emmer",
+        "Flocons d'avoine",
+        "Gluten",
+        "Gerste",
+        "Gerstenmaiz",
+        "Gerstenmalz",
+        "Gerstenmalzextrakt",
+        "Gerstenmalzmehl",
+        "Grünkern",
+        "Hafer",
+        "Haferclocken",
+        "Haferflocken",
+        "Haferkleie",
+        "Hafervollkorn",
+        "Hafervollkornflocken",
+        "Hafervollkornmehl",
+        "Hartweizen",
+        "Hartweizengreiss",
+        "Hartweizengrie",
+        "Hartweizengriess",
+        "Hartweizengrieß",
+        "Hartweizengrieß*",
+        "Hartweizengrieβ",
+        "Hartweizengriß",
+        "Hartweizendunst",
+        "Hartweizengrütze",
+        "Hartwiezen",
+        "Harweizengrieß",
+        "Harwiezengriess",
+        "Kamut",
+        "Roggen",
+        "Roggenmehl",
+        "Roggensauerteig",
+        "Roggenschrot",
+        "Roggenvollkornmehl",
+        "Roggenvollkornschrot",
+        "Seitan",
+        "Triticale",
+        "UrDinkelgreiss",
+        "Urdinkel-Mehl",
+        "Urdinkelgriess",
+        "Urdinkelmehl",
+        "Urdinkelschrol",
+        "Volkornroggen",
+        "Vollkorn",
+        "Vollkorngerste",
+        "Vollkornhafer",
+        "Weizen",
+        "Weizeneiweiss",
+        "Weizeneiweiss",
+        "Weizeneiweiß",
+        "Weizeneiweiß",
+        "Weizenflocken",
+        "Weizengluten",
+        "Weizengriess",
+        "Weizengrieß",
+        "Weizenhalbweissmehl",
+        "Weizenhalbweißmehl",
+        "Weizenkeim",
+        "Weizenkeime",
+        "Weizenkleber",
+        "Weizenkleber",
+        "Weizenkleie",
+        "Weizenkorn",
+        "Weizenkörner",
+        "Weizenmehl",
+        "Weiizenmehl",
+        "Welzenmehl",
+        "Weizenstärke"
+      ],
+      "filter_warning": [
+        "Sojasoße",
+        "Sojasosse",
+        "Sojasoßenpulver",
+        "Sojasossenpulver"
+      ]
     },
-	{
-		name: "Laktose",
-		  "filter_exclusion": [
-			"Kondens[a-zA-Z]*milch", "Butter", "Kuhmilch", "Bergmilch", "Schafmilch", "Büffelmilch", "Ziegenmilch", "Buttermilchpulver", "Buttermilch", "Lactitol", "Laktit", "Entrahmte Milch", "Kefir", "Kefirpulver", "Lactose Monohydrat", "Joghurt", "Yoghurt", "Magerjoghurtpulver", "Joghurtpulver", "Yoghurtpulver", "Magermilch", "Magermilchpulver", "Magermilchjoghurt", "Milch", "Milcherzeugnis", "Milchpulver", "Milchzubereitung", "Milchzucker", "Vollmilchjoghurt", "Laktose", "Lactose", "Molke", "Molkenerzeugnisse", "Molkenpulver", "Molkepulver", "Molkenproteinkonzentrat", "Rahm", "Doppelrahm", "Deppelrahm", "Rahmpulver", "Kaffeerahm", "Kaffeerrahm", "Kafeerahm", "Halbrahm", "Vollrahm", "Rehmpulver", "Sauerrahm", "Sauerrahmpulver", "Kefferahm", "Sahne", "Kaffeesahne", "Quark", "Magerquark", "Speisequark", "Sauermolke", "Sauermolkepulver", "Süßmolke", "Süssmolke", "Süßmolkenpulver", "Süssmolkenpulver", "Vollmilch", "Vollmilchpulver", "Frischkäse", "Doppelrahmfrischkäse", "Käse", "Kase", "Schmelzkase", "Schmelzkäse", "Hartkäse", "Parmigiano Reggiano", "Mozzarella", "Frankendammer", "Edamer", "Emmentaler", "Extrahartkäse", "Gruyère", "Gruyere", "Halbhartkäse"
-		  ],
-		  "filter_warning": [
-			"Buttereinfett", "Natriumstearoyl-2-lactylat", "E481", "Milcheiweiß", "Milcheiweiss", "Milchprotein"
-		  ]
-	},
-	{
-		name: "Schalenfrüchte",
-		  "filter_exclusion": [
-			"Schalenfrüchte", "Nüsse", "Mandeln", "Mandel", "Haselnüsse", "Haselnusse", "Haselnuss", "Haselnussstücke", "Haselnusspaste", "Haselnussmus", "Haselnusskerne", "Walnüsse", "Walnuuss", "Walnusskerne", "Kaschunüsse", "Kaschunuss", "Cashewnuss", "Cashewnüsse", "Cashewkerne", "Cashenüsse", "Pekannüsse", "Pekannuuss", "Paranüsse", "Paranuss", "Paranusskerne", "Pistazien", "Pistazie", "Pistazienpaste", "Macadamia", "Macadamianuss", "Macadamianüsse", "Queenslandnüsse ", "Queenslandnuss ", "Marzipan", "Pinienkernen", "Pinienkern", "Pinienkerne"
-		  ],
-		  "filter_warning": [
-			"Mortadella"
-		  ]
-	},
-	{
-		name: "Erdnüsse",
-		  "filter_exclusion": [
-			"Erdnuss", "Erdnüsse", "Erdnusskerne", "Erdnüsskerne"
-		  ],
-		  "filter_warning": [
+    {
+      name: "Laktose",
+      "filter_exclusion": [
+        "Kondens[a-zA-Z]*milch", "Butter", "Kuhmilch", "Bergmilch", "Schafmilch", "Büffelmilch", "Ziegenmilch", "Buttermilchpulver", "Buttermilch", "Lactitol", "Laktit", "Entrahmte Milch", "Kefir", "Kefirpulver", "Lactose Monohydrat", "Joghurt", "Yoghurt", "Magerjoghurtpulver", "Joghurtpulver", "Yoghurtpulver", "Magermilch", "Magermilchpulver", "Magermilchjoghurt", "Milch", "Milcherzeugnis", "Milchpulver", "Milchzubereitung", "Milchzucker", "Vollmilchjoghurt", "Laktose", "Lactose", "Molke", "Molkenerzeugnisse", "Molkenpulver", "Molkepulver", "Molkenproteinkonzentrat", "Rahm", "Doppelrahm", "Deppelrahm", "Rahmpulver", "Kaffeerahm", "Kaffeerrahm", "Kafeerahm", "Halbrahm", "Vollrahm", "Rehmpulver", "Sauerrahm", "Sauerrahmpulver", "Kefferahm", "Sahne", "Kaffeesahne", "Quark", "Magerquark", "Speisequark", "Sauermolke", "Sauermolkepulver", "Süßmolke", "Süssmolke", "Süßmolkenpulver", "Süssmolkenpulver", "Vollmilch", "Vollmilchpulver", "Frischkäse", "Doppelrahmfrischkäse", "Käse", "Kase", "Schmelzkase", "Schmelzkäse", "Hartkäse", "Parmigiano Reggiano", "Mozzarella", "Frankendammer", "Edamer", "Emmentaler", "Extrahartkäse", "Gruyère", "Gruyere", "Halbhartkäse"
+      ],
+      "filter_warning": [
+        "Buttereinfett", "Natriumstearoyl-2-lactylat", "E481", "Milcheiweiß", "Milcheiweiss", "Milchprotein"
+      ]
+    },
+    {
+      name: "Schalenfrüchte",
+      "filter_exclusion": [
+        "Schalenfrüchte", "Nüsse", "Mandeln", "Mandel", "Haselnüsse", "Haselnusse", "Haselnuss", "Haselnussstücke", "Haselnusspaste", "Haselnussmus", "Haselnusskerne", "Walnüsse", "Walnuuss", "Walnusskerne", "Kaschunüsse", "Kaschunuss", "Cashewnuss", "Cashewnüsse", "Cashewkerne", "Cashenüsse", "Pekannüsse", "Pekannuuss", "Paranüsse", "Paranuss", "Paranusskerne", "Pistazien", "Pistazie", "Pistazienpaste", "Macadamia", "Macadamianuss", "Macadamianüsse", "Queenslandnüsse ", "Queenslandnuss ", "Marzipan", "Pinienkernen", "Pinienkern", "Pinienkerne"
+      ],
+      "filter_warning": [
+        "Mortadella"
+      ]
+    },
+    {
+      name: "Erdnüsse",
+      "filter_exclusion": [
+        "Erdnuss", "Erdnüsse", "Erdnusskerne", "Erdnüsskerne"
+      ],
+      "filter_warning": [
 
-		  ]
-	}
+      ]
+    }
 
   ]
 };
